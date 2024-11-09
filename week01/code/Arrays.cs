27828e80dc6d7f225ef29f7b2c21854154a03435
l-gomes-ar <lgaraujo021@gmail.com>
2024-11-09 03:49:22 +0000

    Week 01: Dynamic Arrays Project Completed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,17 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Declare a variable for an array of length-elements
+        double[] multiples = new double[length];
 
-        return []; // replace this return statement with your own
+        // Start a for loop that iterates length-times, with i as 1
+        // Multiply number by i through each interaction, and add to the array at the [i - 1] position (because arrays are 0 indexed)
+        for (int i = 1; i <= length; i++) {
+            multiples[i - 1] = number * i;
+        }
+
+        // Return the array.
+        return multiples;
     }
 
     /// <summary>
@@ -25,9 +30,12 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Get Range to be rotated and insert it at the start of data (index 0)
+        // "data.Count - amount" gives the starting index to get the elements from and
+        // "amount" equals the count of elements to be rotated
+        data.InsertRange(0, data.GetRange(data.Count - amount, amount));
+
+        // Remove elements rotated from right side of data
+        data.RemoveRange(data.Count - amount, amount);
     }
 }
